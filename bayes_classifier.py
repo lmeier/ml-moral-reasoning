2b28aee90b5ece1754b182128731000e1181e09b
Liam Meier <lmeier@172-18-36-64.DYNAPOOL.NYU.EDU>
2018-04-15 19:22:52 -0400

    better

--- bayes_classifier.py
+++ bayes_classifier.py
@@ -10,21 +10,17 @@ from sklearn.metrics import f1_score
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
 import sklearn.svm
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+
 ## const
 PKL_PATH = "consPapersNew.pkl"
-PKL_PATH1 = "deonPapersNew.pkl"
 
-#PKL_PATH = "JSTORconsPapers.pkl"
-#PKL_PATH1 = "JSTORdeonPapers.pkl"
 
 
 LEMMA_FILTER = ['NOUN', 'PROPN', 'VERB', 'ADJ', 'NUM'] # keep 'NUM' as the last, function lemma_dep_list() refers to this
 def loadDataset(pklPath=PKL_PATH):
     with open(pklPath, "rb") as pklFile:
         return np.array(pickle.load(pklFile, encoding="utf-8"))
-
-c_data_set = loadDataset()
-d_data_set = loadDataset(PKL_PATH1)
 
 def splitData(startString, dataSet, divisions=100):
     newString = startString
@@ -42,27 +38,50 @@ def splitData(startString, dataSet, divisions=100):
     newList.append(temp)
     return newList
 
-c_data = splitData("",c_data_set)
-d_data = splitData("",d_data_set)
+c_data = splitData("", loadDataset("consPapersNew.pkl")) + splitData("", loadDataset("JSTORconsPapers.pkl"))
+d_data = splitData("", loadDataset("deonPapersNew.pkl")) + splitData("", loadDataset("JSTORdeonPapers.pkl"))
 final_data_set = c_data  + d_data
 
 
 #implementing n-gram
 bigram_vectorizer = CountVectorizer(ngram_range=(1, 3),token_pattern=r'\b\w+\b', min_df=1)
 analyze = bigram_vectorizer.build_analyzer()
-print(analyze(c_data_set[0]))
+#print(analyze(c_data_set[0]))
 #print(analyze(final_data_set))
+
+
 X = bigram_vectorizer.fit_transform(final_data_set).toarray()
+n_grams = bigram_vectorizer.get_feature_names()
+tfidf = TfidfVectorizer(ngram_range=(1,3))
+X = tfidf.fit_transform(final_data_set).toarray()
+
 y = []
 for i in c_data:
     y.append('cons')
 for i in d_data:
     y.append('deon')
 
+
 #Naive Bayes
-X_train, X_test, y_train, y_test = train_test_split(X, y, random_state = 50)
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state = 99)
 clf = MultinomialNB().fit(X_train, y_train)
 predicted = clf.predict(X_test)
+
+#visualizing coefficients
+coef = clf.coef_[0].tolist()
+print(len(coef))
+top = 100
+predictors = []
+for i in range(top):
+    val = min(coef)
+    index = coef.index(val)
+    predictors.append([n_grams[index], val])
+    coef.pop(index)
+for i in predictors:
+    print (i ,"\n")
+
+#check accuracy of NB
+
 n = 0
 correct = 0
 for i, j in zip(y_test, predicted):
@@ -85,3 +104,4 @@ for i, j in zip(y_test, predicted):
     if i == j:
         correct = correct + 1
 print(correct*100/n)
+'''
